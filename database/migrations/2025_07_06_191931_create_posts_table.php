81220ceafcb19c1aed209a9fea06d1ee93a885ab
ALEKSANDR.I <alenamobilebkru@gmail.com>
2025-07-07 00:14:00 +0400

    https://github.com/AlexRMB1455AL/laravel_php_rmb.git

--- database/migrations/2025_07_06_191931_create_posts_table.php
+++ database/migrations/2025_07_06_191931_create_posts_table.php
@@ -14,14 +14,16 @@ return new class extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-//            $table->text('content');
-//            $table->unsignedBigInteger('likes')->default(0);
-//            $table->boolean('is_published')->default(false);
-//            $table->
+            $table->text('content');
+            $table->unsignedBigInteger('likes')->default(0);
+            $table->boolean('is_published')->default(false);
+            $table->string('author')->nullable();
             $table->timestamps();
+            $table->string('uber')->nullable();
         });
     }
-
+//php artisan migrate
+//php artisan migrate:rollback
     /**
      * Reverse the migrations.
      */
